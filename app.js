95dc059864edb4216be3db1a4ddf4fbeede97e20
kamrul50c <khasan50c03@gmail.com>
2024-09-15 02:21:57 +0600

    login logoup in navbar

--- app.js
+++ app.js
@@ -70,6 +70,8 @@ app.use((req,res,next)=>{
   res.locals.success=req.flash("success");
   res.locals.error=req.flash("error");
   res.locals.deleteMessage =req.flash("dlt");
+  res.locals.curentuser=req.user;
+
   next();
 });
 
