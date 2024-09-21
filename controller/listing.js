8f6179c720c994584610e998d4c274c6fbc207b3
kamrul50c <khasan50c03@gmail.com>
2024-09-21 09:49:57 +0600

    MVC -> MODEL VIEW AND CONTROLLER  for listing ,, re format

---
+++ controller/listing.js
@@ -0,0 +1,83 @@
+const listening=require("../models/listening.js");
+
+
+
+//flash err function
+  let flasherr=(value, msg,req,res)=>{
+      if(!value){
+        req.flash("error",msg);
+        res.redirect("/index");
+      }
+  }
+  
+
+  module.exports.root=(req, res) => {
+    res.render("root.ejs");
+  };
+
+
+module.exports.index=async (req, res) => {
+    let list = await listening.find();
+    res.render("home.ejs", { list });
+  };
+
+
+
+  module.exports.show=async (req, res) => {
+    let { id } = req.params;
+    const product = await listening.findById(id).populate(
+      {
+        path:"review",
+        populate:{
+          path:"Author",
+        },
+    })
+    .populate("owner");
+    
+    flasherr(product ,"The listing you want to show doesn't exist",req,res);
+    res.render("show.ejs", { product });
+  };
+
+
+
+  module.exports.newlistiong=(req, res) => {
+    
+    res.render("create.ejs");
+  };
+
+
+  module.exports.create=async (req, res, next) => {
+    let newproduct = new listening(req.body);
+    newproduct.owner=req.user._id;
+    await newproduct.save();
+    req.flash("success", "New listing created!");
+    res.redirect("/index");
+  };
+
+  module.exports.destroy=async (req, res) => {
+    let { id } = req.params;
+    await listening.findByIdAndDelete(id);
+    req.flash("dlt","succesfully delete The listing!");
+    res.redirect("/index");
+  };
+
+
+  module.exports.edit=async (req, res) => {
+    let { id } = req.params;
+    const currentproduct = await listening.findById(id);
+    flasherr(currentproduct,"The listing you have search dosen't exist",req,res);
+    res.render("edit.ejs", { currentproduct });
+  };
+
+  module.exports.update=async (req, res) => {
+    let { id } = req.params;
+    let updateData = req.body;
+    await listening.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+    req.flash("success","listing is updated");
+
+    res.redirect(`/show/${id}`);
+  };
+
--- routes/listing.js
+++ routes/listing.js
@@ -7,128 +7,46 @@ const Cerror = require("../utility/ExpressError.js");
 const productSchema = require("../validateSchema/productSchema.js");
 const {islogin, isOwner}= require("./middleware/isauthenticate.js");
 const user=require("../models/user.js")
+const controller=require("../controller/listing.js")
 
 //validateproduct function
 const validateproduct = (req, res, next) => {
-    const { error } = productSchema.validate(req.body);
-    if (error) {
-      let msgerr=error.details.map((el)=>el.message).join(",")
-      throw new Cerror(400, msgerr); // Handle validation error
-    } else {
-      next();
-    }
-  };
-//flash err function
-  let flasherr=(value, msg,req,res)=>{
-      if(!value){
-        req.flash("error",msg);
-        res.redirect("/index");
-      }
+  const { error } = productSchema.validate(req.body);
+  if (error) {
+    let msgerr=error.details.map((el)=>el.message).join(",")
+    throw new Cerror(400, msgerr); // Handle validation error
+  } else {
+    next();
   }
-  
+};
+
   //root route
   
-  route.get("/", (req, res) => {
-    res.render("root.ejs");
-  });
+  route.get("/",controller.root);
   
   //home route
-  route.get(
-    "/index",
-    wrap_async(async (req, res) => {
-      let list = await listening.find();
-      res.render("home.ejs", { list });
-    })
-  );
+  route.get("/index",wrap_async(controller.index));
   
   //show route
   
-  route.get(
-    "/show/:id",
-    wrap_async(async (req, res) => {
-      let { id } = req.params;
-      const product = await listening.findById(id).populate(
-        {
-          path:"review",
-          populate:{
-            path:"Author",
-          },
-      })
-      .populate("owner");
-      
-      flasherr(product ,"The listing you want to show doesn't exist",req,res);
-      res.render("show.ejs", { product });
-    })
-  );
+  route.get("/show/:id", wrap_async(controller.show));
   
   // new
   
-  route.get("/new",
-    islogin,
-    (req, res) => {
-    
-    res.render("create.ejs");
-  });
+  route.get("/new",islogin,controller.newlistiong);
   
   //create
-  route.post(
-    "/create",
-    islogin,
-    validateproduct,
-    wrap_async(async (req, res, next) => {
-      let newproduct = new listening(req.body);
-      newproduct.owner=req.user._id;
-      await newproduct.save();
-      req.flash("success", "New listing created!");
-      res.redirect("/index");
-    })
-  );
+  route.post("/create",islogin,validateproduct,wrap_async(controller.create) );
   
   // /delete route
   
-  route.delete(
-    "/delete/:id",
-    islogin,
-    isOwner,
-    wrap_async(async (req, res) => {
-      let { id } = req.params;
-      await listening.findByIdAndDelete(id);
-      req.flash("dlt","succesfully delete The listing!");
-      res.redirect("/index");
-    })
-  );
+  route.delete("/delete/:id", islogin,isOwner,wrap_async(controller.destroy));
   
   //update route
   
-  route.get(
-    "/edit/:id",
-    islogin,
-    isOwner,
-    wrap_async(async (req, res) => {
-      let { id } = req.params;
-      const currentproduct = await listening.findById(id);
-      flasherr(currentproduct,"The listing you have search dosen't exist",req,res);
-      res.render("edit.ejs", { currentproduct });
-    })
-  );
+  route.get( "/edit/:id", islogin, isOwner,wrap_async(controller.edit) );
   
-  route.put(
-    "/update/:id",
-    islogin,
-    isOwner,
-    validateproduct,
-    wrap_async(async (req, res) => {
-      let { id } = req.params;
-      let updateData = req.body;
-      await listening.findByIdAndUpdate(id, updateData, {
-        new: true,
-        runValidators: true,
-      });
-      req.flash("success","listing is updated");
-  
-      res.redirect(`/show/${id}`);
-    })
-  );
+  route.put("/update/:id", islogin, isOwner,validateproduct, wrap_async(controller.update));
   
 
 
