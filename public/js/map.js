2d6364d71c9bc2ffe1f0d262f936aed9c23c95a0
kamrul50c <khasan50c03@gmail.com>
2024-09-24 02:11:26 +0600

    add marker in map

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,18 @@
+// Get your own API Key on https://myprojects.geoapify.com
+var myAPIKey = mapToken;
 
-          // Get your own API Key on https://myprojects.geoapify.com
-          var myAPIKey = mapToken;
-          var map = new maplibregl.Map({
-              container: 'my-map',
-              style: `https://maps.geoapify.com/v1/styles/klokantech-basic/style.json?apiKey=${myAPIKey}`,
-              center: [0, 0], // Set initial map center [lng, lat]
-              zoom: 2 // Set initial zoom level
-          });
-  
-          map.addControl(new maplibregl.NavigationControl());
+var map = new maplibregl.Map({
+    container: 'my-map',
+    style: `https://maps.geoapify.com/v1/styles/klokantech-basic/style.json?apiKey=${myAPIKey}`,
+    center: coordinates, // Set initial map center [lng, lat]
+    zoom: 9 // Set initial zoom level
+});
+
+map.addControl(new maplibregl.NavigationControl());
+
+console.log(coordinates);
+
+// Add a marker to the map
+const marker = new maplibregl.Marker({color:"red"})
+    .setLngLat(coordinates) // Set marker position [lng, lat]
+    .addTo(map);
