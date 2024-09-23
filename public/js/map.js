688c4c2f73cae46a9939a2c2dd4e07d9fef25e9a
kamrul50c <khasan50c03@gmail.com>
2024-09-24 02:40:05 +0600

    map pop set

--- public/js/map.js
+++ public/js/map.js
@@ -4,15 +4,20 @@ var myAPIKey = mapToken;
 var map = new maplibregl.Map({
     container: 'my-map',
     style: `https://maps.geoapify.com/v1/styles/klokantech-basic/style.json?apiKey=${myAPIKey}`,
+    
     center: coordinates, // Set initial map center [lng, lat]
     zoom: 9 // Set initial zoom level
 });
 
 map.addControl(new maplibregl.NavigationControl());
 
-console.log(coordinates);
-
+// Create a popup
+const popup = new maplibregl.Popup()
+    .setHTML("<h3>Location</h3><p>Your specific location description here.</p>"); // Customize the content
 // Add a marker to the map
 const marker = new maplibregl.Marker({color:"red"})
     .setLngLat(coordinates) // Set marker position [lng, lat]
+    .setPopup(popup) // Bind the popup to the marker
     .addTo(map);
+
+    marker.bindPopup("location").openPopup()
